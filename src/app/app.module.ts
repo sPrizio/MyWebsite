074d98c1ee263b29886b8a28c827bd4ae660bcfd
Stephen <s.prizio@hotmail.com>
2017-08-24 01:04:01 -0400

    adds support for material animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,8 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
+  entryComponents: [],
   providers: [],
   bootstrap: [AppComponent]
 })
