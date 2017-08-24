bddac7dbc345fcb6272f59bca0e9dc8016b3f84d
Stephen <s.prizio@hotmail.com>
2017-08-24 14:02:00 -0400

    sw-2: completes styling and structure of welcome hero

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -10,6 +10,22 @@ export class MainViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.controlFontSize();
   }
 
+  //  defaults to fixed font size for larger screens
+  controlFontSize(): void {
+    const title = document.getElementById('page-title');
+    const subtitle = document.getElementById('page-subtitle');
+
+    window.addEventListener('resize', function() {
+      if (window.innerWidth >= 1920) {
+        title.style.fontSize = '4.5em';
+        subtitle.style.fontSize = '2.25em';
+      } else {
+        title.style.fontSize = '3.5vw';
+        subtitle.style.fontSize = '2vw';
+      }
+    });
+  }
 }
