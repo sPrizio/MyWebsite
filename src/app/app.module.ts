8f925016e0ace714568cf17a7e2aa50b4b9d2e90
Stephen <s.prizio@hotmail.com>
2017-08-24 16:05:19 -0400

    sw-2: creates new component for main view sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainViewComponent
+    MainViewComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
