90053c5ba96c439eaf30c63bf8069f380e5a7ec6
Stephen <s.prizio@hotmail.com>
2017-08-26 01:07:07 -0400

    sw-2: deletes previous component, establishes new page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainViewComponent,
-    SectionComponent
   ],
   imports: [
     BrowserModule,
--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main-view',
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
 
-  constructor() { }
+  //  calculates overlay's container size based on image
+  private static calculateOverlaySize(): void {
+    const sizedElement = <HTMLImageElement>document.getElementsByClassName('project-image')[0];
+    const overlays = document.getElementsByClassName('overlay');
+
+    const correctWidth = sizedElement.width;
+    const correctHeight = sizedElement.height;
+
+    for (let i = 0; i < overlays.length; i++) {
+      const temp = <HTMLElement>overlays[i];
+      temp.style.width = String(correctWidth) + 'px';
+      temp.style.height = String(correctHeight) + 'px';
+    }
+  }
+  constructor() {}
 
   ngOnInit() {
     this.controlFontSize();
+    this.attachLoadImageListener();
+    this.controlOverlayWidths();
   }
 
   //  defaults to fixed font size for larger screens
@@ -18,7 +34,7 @@ export class MainViewComponent implements OnInit {
     const title = document.getElementById('page-title');
     const subtitle = document.getElementById('page-subtitle');
 
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
       if (window.innerWidth >= 1920) {
         title.style.fontSize = '4.5em';
         subtitle.style.fontSize = '2.25em';
@@ -27,5 +43,19 @@ export class MainViewComponent implements OnInit {
         subtitle.style.fontSize = '2vw';
       }
     });
+  }
+
+  //  controls widths of overlays
+  controlOverlayWidths(): void {
+    window.addEventListener('resize', MainViewComponent.calculateOverlaySize);
+  }
+
+  //  calculates overlay size on image load
+  attachLoadImageListener(): void {
+    const elements = <HTMLCollectionOf<HTMLImageElement>>document.getElementsByClassName('project-image');
+
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('load', MainViewComponent.calculateOverlaySize);
+    }
   }
 }
