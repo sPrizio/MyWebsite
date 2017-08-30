6ad730a257a6bf4cae72ef77359966b502d3d52d
Stephen <s.prizio@hotmail.com>
2017-08-30 02:15:31 -0400

    sw-8: redesigns portfolio section

--- src/app/portfolio-project/portfolio-project.component.ts
+++ src/app/portfolio-project/portfolio-project.component.ts
@@ -6,10 +6,12 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./portfolio-project.component.scss']
 })
 export class PortfolioProjectComponent implements OnInit {
+  @Input() orientation: string;
   @Input() imageSrc: string;
   @Input() linkHref: string;
   @Input() description: string;
   @Input() title: string;
+  @Input() work: string;
 
   //  calculates overlay's container size based on image
   private static calculateOverlaySize(): void {
