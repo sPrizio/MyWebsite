f2e98e4289c48a935ec923eff7f3f3d31a49fc71
Stephen <s.prizio@hotmail.com>
2017-08-30 15:31:34 -0400

    sw-8: adds new visit buttons, adds conditional visit or download text customization

--- src/app/portfolio-project/portfolio-project.component.ts
+++ src/app/portfolio-project/portfolio-project.component.ts
@@ -42,7 +42,9 @@ export class PortfolioProjectComponent implements OnInit {
     }
   }
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
     PortfolioProjectComponent.attachLoadImageListener();
